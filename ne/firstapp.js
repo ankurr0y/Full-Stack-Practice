c398beae6fe00d47f0d4575d92d7fba6d7425548
ankurr0y <ankur_roy@outlook.com>
2020-06-06 12:01:50 +0545

    Node + Express 5

--- ne/firstapp.js
+++ ne/firstapp.js
@@ -1,16 +1,24 @@
 var express=require('express');
 var app=express();
 app.set('port',process.env.port||3000);
-app.use(function(req,res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404-Not found');
+app.get('/',function(request,response){//Default code is 200
+    response.type('text/html');
+    response.send('Application homepage');
 });
-app.use(function(err,res,req,next){
+app.get('/about',function(request,response){
+    response.type('text/html');
+    response.send('Application about page');
+});
+app.use(function(request,response){
+    response.type('text/plain');
+    response.status(404);
+    response.send('404-Not found');
+});
+app.use(function(err,response,request,next){
     console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500-Internal Error');
+    response.type('text/plain');
+    response.status(500);
+    response.send('500-Internal Error');
 });
 app.listen(app.get('port'),function(){
     console.log("Running in localhost:"+app.get('port')+"Ctrl C to terminate");
