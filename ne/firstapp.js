1a9facaa4dac288f3e419a86a6d732b5bf18e480
ankurr0y <ankur_roy@outlook.com>
2020-06-07 13:04:31 +0545

    Node + Express 7

--- ne/firstapp.js
+++ ne/firstapp.js
@@ -1,5 +1,7 @@
 var express=require('express');
 var app=express();
+app. use(express. static(__dirname + '/public' ));
+var numbers=[1,2,3,172,37,273,328,273,19,93,73,5,83,19,38,73,476];
 var handlebars=require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
@@ -8,7 +10,8 @@ app.get('/',function(request,response){//Default code is 200
     response.render('home');
 });
 app.get('/about',function(request,response){
-    response.render('about');
+    var randNum=numbers[Math.floor(numbers.length*Math.random())];
+    response.render('about',{number:randNum});
 });
 app.use(function(request,response){
     response.status(404);
